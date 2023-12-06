be4a18b9b3b8871890c60a717ffd134d8eb83b14
Harshinde3011 <108318918+Harshinde3011@users.noreply.github.com>
2023-12-06 06:50:15 +0000

    NUmbers in JS

---
+++ 01_basic/Nums.js
@@ -0,0 +1,17 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(10000)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const OtherNumber = 23.8679
+console.log(OtherNumber.toPrecision(3));
+
+const newNumber = 1000000
+console.log(newNumber.toLocaleString());
+console.log(newNumber.toLocaleString('en-IN')); // indian standard 
+
+
