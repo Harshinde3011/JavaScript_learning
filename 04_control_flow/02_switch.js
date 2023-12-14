8734e71f66ed5d1200414f64e5f94658bc178e3a
Harshinde3011 <108318918+Harshinde3011@users.noreply.github.com>
2023-12-14 13:46:37 +0000

    lec 26 Switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,36 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const value = 3
+
+switch(value){
+    case 1:
+        console.log("January");
+        break;
+    
+    case 2:
+        console.log("February");
+        break;
+    
+    case 3:
+        console.log("March");
+        break;
+
+    case 4:
+        console.log("April");
+        break;
+
+    
+    
+    default:
+        console.log("Out of the calender");
+        break;
+}
+// OP = March
+
