9a18dfcfb2a34ce302784c05ebfe63c7b5d67704
Harshinde3011 <108318918+Harshinde3011@users.noreply.github.com>
2023-12-15 05:55:12 +0000

    while and do-while loop

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,37 @@
+// While lopp
+
+//let i = 1
+
+//while (i <= 10) {
+//    console.log(i);
+//    i++
+//}
+
+
+let MyArray = ['harsh','ram','raj','sita','krishna']
+let i =0
+
+while (i < MyArray.length) {
+    console.log(MyArray[i]);
+    i++
+}
+
+
+// do while loop
+
+
+// let score = 1
+
+// do {
+//     console.log(score);
+//     score++
+// } while (score < 10); // 1 2 3 4 5 6 7 8 9 
+
+let score = 11
+ 
+do {
+    console.log(score);
+    score++
+} while (score < 10);
+
+// OP = 11 Do while loop is executed atleast once 
